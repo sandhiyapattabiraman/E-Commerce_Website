0aeb5d6479f5c5470529f6cae2d3dcd9032c4700
sandhiyapattabiraman <psandhiya2304@gmail.com>
2024-11-29 10:33:55 +0530

    changes in category page

---
+++ Assets/pages/js/accessory.js
@@ -0,0 +1,19 @@
+fetch('../../../Assets/pages/json/accessory.json')
+  .then(response => response.json())
+  .then(jsonData => {
+    const productList = document.getElementById("items");
+
+    jsonData.products.forEach(product => {
+      const productDiv = document.createElement("div");
+      productDiv.classList.add("product");
+      productDiv.innerHTML = `
+        <img src="${product.image}" alt="wishlist_img" class="wishlist-img">
+        <img src="${product.image1}" alt="stand_mixer_img" class="product-image">
+        <img src="${product.image2}" alt="rating" class="star_rating">
+        <p>Price: ${product.price}</p>
+        <button type="button" class="Button">Add to Cart</button>
+        <button type="button" class="Buttons">Buy Now</button>
+      `;
+      productList.appendChild(productDiv);
+    });
+  });
